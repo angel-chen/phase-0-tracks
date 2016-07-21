c33ba8cb4985a2ecbd21996d75baf64bc2740119
Angel <rikugou@gmail.com>
2016-07-21 16:52:08 -0400

    Add nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,69 @@
+closet = {
+	shelf: {
+		total_shelves: 3,
+		items: {
+			handbags: 5,
+			backpacks: 2,
+			hats: 3
+		},
+		brand: [
+			'Coach',
+			'Tory Burch',
+			'Kate Spade'
+		]
+	},
+	drawer: {
+		total_drawers: 4,
+		items: {
+			socks: 10,
+			bottoms: 15,
+			tops: 20,
+			dresses: 10,
+		},
+		brand: [
+			'Uniqlo',
+			'Express',
+			'American Eagle'
+		]
+	},
+	garment_rack: {
+		total_gracks: 1,
+		items: {
+			blazer: 2,
+			pants: 5,
+			skirt: 5
+		},
+		brand: [
+			'Uniqlo',
+			'Express',
+			'American Eagle'
+		]
+	}
+	hooks: {
+		total_hooks: 12,
+		items: {
+			scarves: 5,
+			belts: 5
+		},
+		brand: [
+			'American Eagle',
+			'J Crew'
+		]
+	},
+	shoe_rack: {
+		total_racks: 3,
+		items: {
+			sandals: 2,
+			flats: 3,
+			boots: 3,
+			sneakers: 1
+		},
+		brand: [
+			'Tory Burch',
+			'TOMS',
+			'Converse',
+			'Nike',
+			'Sorel'
+		]
+	}
+}
