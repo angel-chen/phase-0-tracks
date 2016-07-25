69a2aa366ffa53c0805353675233ddb2255cb067
Angel <rikugou@gmail.com>
2016-07-25 10:54:51 -0400

    Add modified algorithm and interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,43 +7,48 @@
 	- Change all consonants to next consonant in 'bcdfghjklmnpqrstvwxyz'
 	- For vowel 'u', return 'a'
 	- For consonant 'z', return 'b' 
+	- Add exception for spaces
 	- Capitalize result
 =end
 
-name = "Felicia Torres"
+# name = "Felicia Torres"
 
 def create_alias(name)
 	
 	spy_name = name.split(' ')
 	spy_name[0], spy_name[1] = spy_name[1], spy_name[0]
-	spy_name = spy_name.join(' ').downcase.chars
+	spy_name = spy_name.join(' ').downcase.reverse.chars
 
 	vowel = %w[a e i o u]
 	consonant = %w[b c d f g h j k l m n p q r s t v w x y z]
 
-	i = 0
+	index = 0
 
-		while i < name.length
+	while index < spy_name.length
 
-			spy_name = name[i].chars
-
-			spy_name.each_index{|letter|
-				if each do |letter|
-				
-				# elsif letter == " "
-				# 	letter
-				# elsif letter == "z"
-				# 	letter = "b"
-				# elsif letter == "u"
-				# 	letter = "a"
-				# end
-			index+=1
-		 	end
-		end
+		spy_name.each do |letter|
+			letter.next
+			end
+		index+=1
+	 	end
 end
 
-			
+spy_names = {}
+name = nil
 
+while name != "quit" 
+	puts "Enter a name. Type 'quit' when finished"
+	name = gets.chomp
+
+	if name == "quit"
+		puts "Quitting program"
+		break
+	else
+		create_alias(name)
+		spy_alias = create_alias(name)
+
+		spy_names[:"#{name}"] = spy_alias
+	end
 end
 
-p create_alias(name)
+p spy_names
