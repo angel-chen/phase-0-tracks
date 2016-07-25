5e8f11cbce097caa9f2e2895f739d0f29301db9c
Angel <rikugou@gmail.com>
2016-07-25 14:27:07 -0400

    Add comments for puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ------------------------
+Color: varies
+Hair length: varies
+Friendly: yes
+Name: varies
+Gives kisses: yes
+Paper trained: no
+
+BEHAVIOR ------------------------
+Rambunctious
+Chews on everything
+Always looks cute
+	
+=end
+
