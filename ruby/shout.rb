4beccda44f06be7c040881f97a89f1f54f3ff04d
Angel Chen <rikugou@gmail.com>
2016-07-30 08:55:10 -0400

    Convert standalone module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,35 @@
-module Shout
-  def self.yell_angrily(words)
-    words  + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words  + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words.upcase + "!!!" + " :D"
-  end
+#   def self.yelling_happily(words)
+#   	words.upcase + "!!!" + " :D"
+#   end
+# end
+
+# #DRIVER CODE
+# p Shout.yell_angrily("NO")
+# p Shout.yelling_happily("yay")
+
+module Shout
+	def yelling(words)
+		puts words.upcase + "!!!"
+	end
 end
 
+class Amusement_park
+	include Shout
+end
+
+class Bug_discovery
+	include Shout
+end
+
+
 #DRIVER CODE
-p Shout::yell_angrily("NO")
-p Shout::yelling_happily("yay")
+sixflags = Amusement_park.new
+sixflags.yelling("wheeeeee")
+
+roach = Bug_discovery.new
+roach.yelling("kill it dead")
