80ebf91723b14dd2dd58f731cf99e0d155d56398
Angel Chen <rikugou@gmail.com>
2016-08-01 08:39:16 -0400

    Add pseudocode, modify unhide word method

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,35 +1,75 @@
 #Angel Chen
 #6.6 Solo Challenge
 
+=begin
+On Initialization
+	-Set variable for word to be guessed
+	-Set default variable for game over to be false
+	-Set guess count to word length
+	-Set array for guessed letters
+
+Hiding Word
+	-Get user word
+	-Change word to underscore with spaces
+
+Guessing Word
+	-Check user entered guess against word
+	-Send guessed letter into guessed letter array
+	-If guessed letter is in word, replace underscore
+	-Subtract guess count if guessed letter is wrong
+	-If guessed letter is already in guessed letter array, ask for another letter but do not increase guess count
+	-Game is lost if guesses are more than word length
+
+Interface
+-User can enter a word
+-Word is replaced with _ _ _ _ _ _ _ depending on length of word at beginning of game (unicorn)
+	-Letters start appearing when correct (_ _ _ c _ _ _) - letter C
+-Another user attempts to guess
+-Number of guesses limited to length of the word
+-Guesses stored in array
+-Repeated guesses do not count against the user
+-Congratulatory message if they win - You are clearly a word guessing genius
+-Taunting message if they lose - Better luck next time, loser.
+		
+
+=end
+
 class Hangman
-	attr_reader :is_over, :guess_count, :hidden
-	attr_accessor :word, :guesses
+	attr_reader :is_over, :guess_count
+	attr_accessor :word, :guesses, :hide
 
 	def initialize(word)
 		@word = word
 		@is_over = false
 		@guess_count = word.length
 		@guessed_letters = []
-		@hide = '_ '
 	end
 
 #Hide the word and replace with underscores
 	def hide_word
-		@word = word
-		@hide = @hide * @guess_count
-		@hide = @hide.split(" ").join(" ")
+		@word = word.split("")
+		@hide = @word.map do |letter| 
+			letter = "_"
+		end
 		
 	end
 
 #Unhide word if letter guess is correct
-	def unhide_word
-		 @word.split("").each do |letter|
+	def unhide_word(guess)
+		@word.map do |letter|
 		 	if @guessed_letters.include?(letter)
-		 		
+		 		@hide = letter + " "
 		 	else
-		 		
+		 		@hide = "_"
 		 	end
 		 end
+		# i = 0
+		# while i < @word.length
+		# 	if @word[i] == guess
+		# 		@hide[i] = guess
+		# 	end
+		# 	i+=1
+		# end
 	end
 
 #Check if word has been guessed, letter is in guessed letters or if there are no more guesses left
@@ -61,12 +101,12 @@ end
 # word = gets.chomp
 
 # game = Hangman.new(word)
-# puts game.hide_word
+# puts game.hide_word.join(" ")
 
 # while !game.is_over
 # 	puts "Please guess the word or letter in the word"
 # 	guess = gets.chomp
 
 # 	puts game.check_word(guess)
-# 	puts game.unhide_word
+# 	puts game.unhide_word(guess)
 # end
--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -4,12 +4,12 @@ describe Hangman do
 	let(:game) { Hangman.new("unicorn")}
 
 	it "turns word into series of underscores separated by spaces" do 
-		expect(game.hide_word).to eq "_ _ _ _ _ _ _"
+		expect(game.hide_word.join(" ")).to eq "_ _ _ _ _ _ _"
 	end
 
 	it "changes underscore into correctly guessed letter" do
-		game.check_word("c")
-		expect(game.unhide_word).to eq ["_ _ _ c _ _ _ "]
+		game.check_word("unicorn")
+		expect(game.unhide_word("c")).to eq ["_ _ _ c _ _ _ "]
 	end
 
 	it "gives congratulatory message for winning" do
