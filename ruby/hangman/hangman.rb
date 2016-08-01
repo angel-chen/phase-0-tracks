34c0a1213de70f78e14b0f1fa7e6a24b7333495f
Angel Chen <rikugou@gmail.com>
2016-07-31 23:41:07 -0400

    Add get word method

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -4,6 +4,31 @@
 class Hangman
 	attr_reader :word, :is_over, :guess_count, :guessed_letters, :word_length, :hidden_word
 
+	def initialize
+		puts "Starting Hangman game..."
 
+		@word = ""
+		@is_over = false
+		@guess_count = 0
+		@guessed_letters = nil
+		@word_length = 0
+		@hidden_word = " _ "
+	end
+
+	def get_word(word)
+		@word = word
+		@length = @word.length
+		@hidden_word = @hidden_word * @length
+		@hidden_word = @hidden_word.chop.split(" ")
+		p @hidden_word
+		p @word
+	end
+
+	
 
 end
+
+#DRIVER CODE
+game = Hangman.new
+
+game.get_word("unicorn")
