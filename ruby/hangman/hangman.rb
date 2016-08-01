2e87e8dfebdde2f9caaec5afe09784e150d5bf4a
Angel Chen <rikugou@gmail.com>
2016-08-01 06:22:12 -0400

    Add code to test rspec. 2 out of 3 pass

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -2,33 +2,71 @@
 #6.6 Solo Challenge
 
 class Hangman
-	attr_reader :word, :is_over, :guess_count, :guessed_letters, :word_length, :hidden_word
+	attr_reader :is_over, :guess_count, :hidden
+	attr_accessor :word, :guesses
 
-	def initialize
-		puts "Starting Hangman game..."
-
-		@word = ""
-		@is_over = false
-		@guess_count = 0
-		@guessed_letters = nil
-		@word_length = 0
-		@hidden_word = " _ "
-	end
-
-	def get_word(word)
+	def initialize(word)
 		@word = word
-		@length = @word.length
-		@hidden_word = @hidden_word * @length
-		@hidden_word = @hidden_word.chop.split(" ")
-		p @hidden_word
-		p @word
+		@is_over = false
+		@guess_count = word.length
+		@guessed_letters = []
+		@hide = '_ '
 	end
 
-	
+#Hide the word and replace with underscores
+	def hide_word
+		@word = word
+		@hide = @hide * @guess_count
+		@hide = @hide.split(" ").join(" ")
+		
+	end
+
+#Unhide word if letter guess is correct
+	def unhide_word
+		 @word.split("").each do |letter|
+		 	if @guessed_letters.include?(letter)
+		 		
+		 	else
+		 		
+		 	end
+		 end
+	end
+
+#Check if word has been guessed, letter is in guessed letters or if there are no more guesses left
+	def check_word(guess)
+		if guess == @word
+			@is_over = true
+			p "You are clearly a word guessing genius."
+		elsif @word.include?(guess)
+			@guessed_letters << guess
+			p "Getting closer to solving."
+		elsif @guessed_letters.include?(guess)
+			p "You already tried that letter."
+		elsif 
+			@guess_count -= 1
+			p "Guess another letter."
+		else @guess_count == 0
+			@is_over = true
+			p "Better luck next time, loser."
+		end
+	end
 
 end
 
 #DRIVER CODE
-game = Hangman.new
+# game = Hangman.new("unicorn")
+# game.hide_word
 
-game.get_word("unicorn")
+# puts "Please give me a word to start the game."
+# word = gets.chomp
+
+# game = Hangman.new(word)
+# puts game.hide_word
+
+# while !game.is_over
+# 	puts "Please guess the word or letter in the word"
+# 	guess = gets.chomp
+
+# 	puts game.check_word(guess)
+# 	puts game.unhide_word
+# end
--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -1,21 +1,20 @@
 require_relative 'hangman'
 
 describe Hangman do
-	let(:game) { Hangman.new }
+	let(:game) { Hangman.new("unicorn")}
 
 	it "turns word into series of underscores separated by spaces" do 
-		game.get_word("unicorn")
-		expect(game.get_word("unicorn")).to eq "unicorn"
+		expect(game.hide_word).to eq "_ _ _ _ _ _ _"
 	end
 
 	it "changes underscore into correctly guessed letter" do
-		game.unhide_word("c")
-		expect(game.unhide_word).to eq ["_ _ _ c _ _ _"]
+		game.check_word("c")
+		expect(game.unhide_word).to eq ["_ _ _ c _ _ _ "]
 	end
 
 	it "gives congratulatory message for winning" do
-		game.status("unicorn")
-		expect(game.status).to eq "You are clearly a word guessing genius"
+		game.check_word("unicorn")
+		expect(game.check_word("unicorn")).to eq "You are clearly a word guessing genius."
 	end
 end
 
