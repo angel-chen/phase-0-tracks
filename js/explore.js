9a0ba8e9fbff42ed646a63c8364a901f28431c74
Angel Chen <rikugou@gmail.com>
2016-08-01 18:06:04 -0400

    Add javascript function and test with driver code

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+/*
+
+-Declare "hello" as variable
+-Get length of string minus 1 to get index position of last character
+-Set counter to decrease until it hits 0
+-Add '' to add strings together in one line
+
+*/
+
+function reverse(str) {
+  var space = '';
+  for (var i = str.length - 1; i >= 0; i--)
+    space += str[i];
+  return space;
+}
+
+//DRIVER CODE
+reverse("hello");
+
