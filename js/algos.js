860780f733f74ac3c62812634033eb611bda7308
Angel Chen <rikugou@gmail.com>
2016-08-07 14:54:57 -0400

    Change to different loop and add function for key value match

--- js/algos.js
+++ js/algos.js
@@ -7,16 +7,11 @@ Longest Phrase
 Find a Key-Value Match
 -Declare variables Steve and Tamir
 -Create function that takes 2 variables
--Start with first variable and match against values on other object
--Repeat on 2nd object
+-Use loop to go through properties of an object
 -Match on key-value pair
 -Return true if there's a match
 -Return false if there isn't
 */
-
-var phrase = ["long phrase","longest phrase","longer phrase"];
-var Steve = {name: "Steven", age: 54};
-var Tamir = {name: "Tamir", age: 54};
 
 function longestPhrase (phrase) {
 	var longestPhrase = 0;
@@ -31,13 +26,20 @@ function longestPhrase (phrase) {
 	return longPhrase;
 }
 
-
 function keyValueMatch (val1, val2){
-	for (var i = 0; i < Steve.length; i++){
-		if (Steve.key)
+	for (var key in val1){
+		if (val2[key] == val1[key]) {
+			return true;
+		}
 	}
+	return false;
 }
 
 //DRIVER CODE
+var phrase = ["long phrase","longest phrase","longer phrase"];
+var Steve = {name: "Steven", age: 54};
+var Tamir = {name: "Tamir", age: 54};
+
 console.log(longestPhrase(phrase));
+console.log(keyValueMatch(Steve, Tamir));
 
